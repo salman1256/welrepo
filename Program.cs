b4a513acd97f62fff1ee6bf2a95ce4f38816f254
salman1256 <salman1256@yahoo.com>
2025-08-10 19:40:36 +0530

    After adding Table

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to C#, Jenkins GitHub project");
+int num=12;
+System.Console.WriteLine($"Table of Number{num}");
+for (int i = 1; i <= 10; i++)
+{
+    System.Console.WriteLine($"{num}*{i}=\t{num * i}");
+
+}
+
